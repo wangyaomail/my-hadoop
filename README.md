f0368aebbe22ff0c1dc79b44ceef3f86b3abc886
wangyaomail <wangyaomail@foxmail.com>
2022-03-02 19:11:01 +0800

    20220302-1

--- README.md
+++
@@ -1,2 +0,0 @@
-该工程用于学习和分析hadoop源码。
-- hadoop-0.1来自hadoop 0.1版本，将ant改为maven，依赖库的版本号不变
---
+++ src/main/java/WordCount.java
@@ -0,0 +1,59 @@
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.util.GenericOptionsParser;
+
+import java.io.IOException;
+
+public class WordCount {
+    public static void main(String[] args) throws Exception {
+        Configuration conf = new Configuration();
+        String[] otherArgs = new GenericOptionsParser(conf, args).getRemainingArgs();
+        if (otherArgs.length < 2) {
+            System.err.println("Usage: wordcount <in> [<in>...] <out>");
+            System.exit(2);
+        }
+        Job job = Job.getInstance(conf, "word count");
+        job.setJarByClass(WordCount.class);
+        job.setMapperClass(WCMapper.class);
+        job.setCombinerClass(WCReducer.class);
+        job.setReducerClass(WCReducer.class);
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(IntWritable.class);
+        for (int i = 0; i < otherArgs.length - 1; ++i) {
+            FileInputFormat.addInputPath(job, new Path(otherArgs[i]));
+        }
+        FileOutputFormat.setOutputPath(job, new Path(otherArgs[otherArgs.length - 1]));
+        System.exit(job.waitForCompletion(true) ? 0 : 1);
+    }
+
+    private static class WCMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
+        @Override
+        public void map(LongWritable key, Text value, Context context)
+                throws IOException, InterruptedException {
+            String[] toks = value.toString().trim().split(" ");
+            for (int i = 0; i < toks.length; i++) {
+                context.write(new Text(toks[i]), new IntWritable(1));
+            }
+        }
+    }
+
+    private static class WCReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
+        @Override
+        public void reduce(Text key, Iterable<IntWritable> values, Context context)
+                throws IOException, InterruptedException {
+            int count = 0;
+            for (IntWritable value : values) {
+                count++;
+            }
+            context.write(key, new IntWritable(count));
+        }
+    }
+}
