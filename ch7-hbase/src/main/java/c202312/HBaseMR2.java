005eb0befcb05f9d69bd5b043039cabff20e6232
yao wang <wangyaomail@foxmail.com>
2023-05-30 12:54:01 +0800

    20230530

---
+++ ch7-hbase/src/main/java/c202312/HBaseMR2.java
@@ -0,0 +1,85 @@
+package c202312;
+import ch7.CH723AgeGenerate;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.hbase.CellUtil;
+import org.apache.hadoop.hbase.HBaseConfiguration;
+import org.apache.hadoop.hbase.TableName;
+import org.apache.hadoop.hbase.client.Mutation;
+import org.apache.hadoop.hbase.client.Put;
+import org.apache.hadoop.hbase.client.Result;
+import org.apache.hadoop.hbase.client.Scan;
+import org.apache.hadoop.hbase.io.ImmutableBytesWritable;
+import org.apache.hadoop.hbase.mapreduce.TableMapReduceUtil;
+import org.apache.hadoop.hbase.mapreduce.TableMapper;
+import org.apache.hadoop.hbase.mapreduce.TableReducer;
+import org.apache.hadoop.hbase.util.Bytes;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+
+import java.io.IOException;
+
+public class HBaseMR2 {
+    public static void main(String[] args) throws IOException, InterruptedException, ClassNotFoundException {
+        String hadoop_home = "C:\\hadoop\\hadoop-3.2.2";
+        System.setProperty("hadoop.home.dir", hadoop_home);
+        System.load(hadoop_home + "/bin/hadoop.dll");
+        Configuration conf = HBaseConfiguration.create();
+        Job job = Job.getInstance(conf, "myjob");
+        job.setJarByClass(CH723AgeGenerate.class);
+        Scan scan = new Scan();
+        scan.addColumn(Bytes.toBytes("data"), Bytes.toBytes("score"));
+        TableMapReduceUtil.initTableMapperJob(TableName.valueOf("students"),
+                scan,
+                MyMapper.class,
+                Text.class, Text.class,
+                job);
+        TableMapReduceUtil.initTableReducerJob("result",
+                MyReducer.class,
+                job);
+        System.exit(job.waitForCompletion(true) ? 0 : 1);
+    }
+    private static class MyMapper extends TableMapper<Text, Text> {
+        int maxScore=-1, minScore=Integer.MAX_VALUE;
+        @Override
+        protected void map(ImmutableBytesWritable key, Result value, Mapper<ImmutableBytesWritable, Result, Text, Text>.Context context) throws IOException, InterruptedException {
+            String scoreStr = Bytes.toString(CellUtil.cloneValue(value.getColumnLatestCell(Bytes.toBytes("data"), Bytes.toBytes("score"))));
+            int score = Integer.parseInt(scoreStr);
+            if(score>maxScore){
+                maxScore=score;
+            }
+            if(score<minScore){
+                minScore=score;
+            }
+         }
+        @Override
+        protected void cleanup(Mapper<ImmutableBytesWritable, Result, Text, Text>.Context context) throws IOException, InterruptedException {
+            context.write(new Text("1"), new Text(maxScore+""));
+            context.write(new Text("1"), new Text(minScore+""));
+        }
+    }
+
+    private static class MyReducer extends TableReducer<Text, Text, ImmutableBytesWritable> {
+        @Override
+        protected void reduce(Text key, Iterable<Text> values, Reducer<Text, Text, ImmutableBytesWritable, Mutation>.Context context) throws IOException, InterruptedException {
+            int maxScore=-1, minScore=Integer.MAX_VALUE;
+            for (Text val : values) {
+                int score = Integer.parseInt(val.toString());
+                if(score>maxScore){
+                    maxScore=score;
+                }
+                if(score<minScore){
+                    minScore=score;
+                }
+            }
+            Put put = new Put(Bytes.toBytes("max"));
+            put.addColumn(Bytes.toBytes("data"), Bytes.toBytes("score"), Bytes.toBytes(maxScore+""));
+            context.write(new ImmutableBytesWritable(), put);
+
+            put = new Put(Bytes.toBytes("min"));
+            put.addColumn(Bytes.toBytes("data"), Bytes.toBytes("score"), Bytes.toBytes(minScore+""));
+            context.write(new ImmutableBytesWritable(), put);
+        }
+    }
+}
