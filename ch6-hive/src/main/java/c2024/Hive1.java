53e7e01c097090eb81c0ba07001a73083476311e
wangyaomail <wangyaomail@foxmail.com>
2024-05-20 22:33:36 +0800

    20240520

---
+++ ch6-hive/src/main/java/c2024/Hive1.java
@@ -0,0 +1,25 @@
+package c2024;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class Hive1 {
+    public static void main(String[] args) throws Exception {
+        Class.forName("org.apache.hive.jdbc.HiveDriver");
+        String url = "jdbc:hive2://192.168.17.150:10000/zzti";
+        Connection conn = DriverManager.getConnection(url);
+        Statement stmt = conn.createStatement();
+//        stmt.execute("create table if not exists zzti.students(name string)");
+
+//        stmt.execute("insert into zzti.students values('zhangsan')");
+
+        ResultSet rs = stmt.executeQuery("select * from zzti.students");
+        while (rs.next()) {
+            System.out.println(rs.getString("name"));
+        }
+
+        conn.close();
+    }
+}
