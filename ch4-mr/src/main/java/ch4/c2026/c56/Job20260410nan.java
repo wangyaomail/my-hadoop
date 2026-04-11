9f1ce6447b94477b4cf36538c37c0383f6b50ef8
wangyaomail <wangyaomail@foxmail.com>
2026-04-11 10:33:25 +0800

    20260411

---
+++ ch4-mr/src/main/java/ch4/c2026/c56/Job20260410nan.java
@@ -0,0 +1,54 @@
+package ch4.c2026.c56;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+import java.io.IOException;
+
+public class Job20260410nan {
+    static class MyMapper extends Mapper<LongWritable, Text, Text, Text> {
+        @Override
+        protected void map(LongWritable key, Text value, Mapper<LongWritable, Text, Text, Text>.Context context) throws IOException, InterruptedException {
+            String[] toks = value.toString().trim().split("\t");
+//            if(toks.length==8){
+                if(toks[3].equals("男")){
+                    context.write(new Text(toks[0]), new Text(""));
+                }
+//            }
+        }
+    }
+
+    static class MyReducer extends Reducer<Text, Text, Text, Text> {
+        @Override
+        protected void reduce(Text key, Iterable<Text> values, Reducer<Text, Text, Text, Text>.Context context) throws IOException, InterruptedException {
+            for (Text val : values) {
+                context.write(key,val);
+                break;
+            }
+        }
+    }
+    public static void main(String[] args) throws IOException, InterruptedException, ClassNotFoundException {
+        String hadoop_home = "C:\\hadoop\\hadoop-3.2.2";
+        System.setProperty("hadoop.home.dir", hadoop_home);
+        System.load(hadoop_home + "/bin/hadoop.dll");
+
+        Configuration conf = new Configuration();
+        Job job = Job.getInstance(conf, Job20260410nan.class.getSimpleName());
+        job.setJarByClass(Job20260410nan.class);
+        job.setMapperClass(MyMapper.class);
+        job.setReducerClass(MyReducer.class);
+        job.setMapOutputKeyClass(Text.class);
+        job.setMapOutputValueClass(Text.class);
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(Text.class);
+        FileInputFormat.addInputPath(job, new Path("C:\\nos\\my-hadoop\\data\\students_10w.data"));
+        FileOutputFormat.setOutputPath(job, new Path("C:\\nos\\my-hadoop\\output\\"+ Job20260410nan.class.getSimpleName()));
+        System.exit(job.waitForCompletion(true) ? 0 : 1);
+    }
+}
